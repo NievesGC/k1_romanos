6608bcf89eb45d614146eb0e4eedda8392f536cc
Nieves <niieves99@gmail.com>
2023-05-25 23:25:24 +0200

    final karra 6

--- test_class_romannumbers.py
+++ test_class_romannumbers.py
@@ -1,22 +1,25 @@
-
-
+from cromannumbers import RomanNumber
 
 def test_instanciar_un_numero_romano():
     romano = RomanNumber(23)
-    assert romano.valor == 23
-    assert romano.simbolo == "XXIII"
-    
-"""
-class Alumna:
-    def __init__(self, n, a ):
-        self.nombre = n
-        self.apellido = a
+    assert romano.numero == 23
+    assert romano._simbolo == "XXIII"
 
+def test_instanciar_otro():
+    romano = RomanNumber(13)
+    assert romano.numero == 13
+    assert romano._simbolo == "XIII"
 
+def test_instanciar_con_simbolos():
+    romano = RomanNumber("XI")
+    assert romano.numero == 11
+    assert romano._simbolo == "XI"
 
+def test_cambiar_valor_de_romano():
+    romano  = RomanNumber(1)
+    assert romano.numero == 1
+    assert romano._simbolo == "I"
 
-def test_irene_existe():
-    irene = Alumna("Irene","Bravo")
-    assert irene.nombre == "Irene"
-    assert irene.apellido == "Bravo"
-"""
+    romano.numero = 2
+    assert romano.numero == 2
+    assert romano._simbolo == "II"
