4c48fb726ebffcffb913e54cb3adfaf11d321cbc
Nieves <niieves99@gmail.com>
2023-05-29 22:12:19 +0200

    Multiplica y compara en igualdad

--- cromannumbers.py
+++ cromannumbers.py
@@ -21,3 +21,43 @@ class RomanNumber:
     def numero(self,entrada):
         self._numero = entrada
         self._simbolo = entero_a_romano(entrada)
+
+    @property
+    def simbolo(self):
+        return self._simbolo
+    
+    @simbolo.setter
+    def simbolo(self,entrada):
+        self._simbolo = entrada
+        self._numero = romano_a_entero(entrada)
+
+
+        """
+    metodos magicos para logica
+        """
+
+    def __eq__(self,other):
+        return self.numero == other.numero 
+    
+    """
+    metodos magicos para aritmética
+    """
+
+    def __mul__(self, otro):
+        if not isinstance(otro,RomanNumber):
+            otro = RomanNumber(otro)
+        resultado = self.numero * otro.numero
+        return RomanNumber(resultado)
+    
+    def __rmul__(self, otro):
+        return self.__mul__(otro)
+    
+    """
+    metodos magicos para aritmética
+    """
+
+    def __repr__(self):
+        return f"{self.numero}-{self.simbolo}"
+    
+    def __str__(self):
+        return self.__repr__()
